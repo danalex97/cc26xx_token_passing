b6c24521f628298b5eda7bb898c3fd36eccb7255
Alexandru Dan <dan.alex97@yahoo.com>
2018-02-23 10:15:34 +0000

    Added random packet sending.

--- sender.c
+++ sender.c
@@ -28,7 +28,7 @@ void getPriorityPacket(uint8_t* packet){
 void broadcast_message(uint8_t* packet){
   packetbuf_copyfrom(packet, sizeof(packet));
   broadcast_send(&broadcast);
-#if DEBUG_ENABLED 
+#if DEBUG_ENABLED
   printf("broadcast message sent: %d\n", count);
 #endif
 }
@@ -38,15 +38,15 @@ broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
 #if ENABLE_PRIORITY_PACKET
   uint16_t *datapacket = (uint16_t *)packetbuf_dataptr();
-  uint16_t nodeid = linkaddr_node_addr.u8[1]*256 + linkaddr_node_addr.u8[0];  
+  uint16_t nodeid = linkaddr_node_addr.u8[1]*256 + linkaddr_node_addr.u8[0];
 
   // When overhear a Priority request, check if this is for itself.
   if(datapacket[0] == PRIORITY_REQUEST && datapacket[1] == nodeid){
-#if DEBUG_ENABLED     
+#if DEBUG_ENABLED
     printf("Received priority request with data packet %u for node_id %u\n", datapacket[1], nodeid);
 #endif
     getPriorityPacket(packet);
-    broadcast_message(packet);      
+    broadcast_message(packet);
   }
 #endif
 }
@@ -57,30 +57,43 @@ static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 void getNextPacket(uint8_t* packet){
   packet[1] = count / 256;
   packet[0] = count % 256;
-  count++;  
+  count++;
   if(count == PRIORITY_RESPONSE)
     count = 1;
 }
 
 /*---------------------------------------------------------------------------*/
+// Timer for random message wait.
+static struct ctimer message_wait;
+
+static void
+send_broadcast(void *ptr){
+  getNextPacket(packet);
+  broadcast_message(packet);
+}
+
+/*---------------------------------------------------------------------------*/
 PROCESS_THREAD(sender_mote_process, ev, data)
 {
-  static struct etimer et_periodic;  
+  static struct etimer et_periodic;
 
   PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
 
   PROCESS_BEGIN();
 
   broadcast_open(&broadcast, 129, &broadcast_call);
-  
+
   /* Set Timer*/
   etimer_set(&et_periodic, CLOCK_SECOND/5);
-  
+
   while(1) {
     PROCESS_YIELD();
     if(ev == PROCESS_EVENT_TIMER && data == &et_periodic){
-      getNextPacket(packet);
-      broadcast_message(packet);
+      // broadcast after random timeout
+      uint16_t wait_time = random_rand() >> 9;
+      ctimer_set(&message_wait, wait_time, send_broadcast, NULL);
+
+      // Wait for next event
       etimer_reset(&et_periodic);
     }
   }
